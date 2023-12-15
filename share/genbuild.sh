6c354b7eea9bbc8397d7f596efcf822879f2376e
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2023-12-15 01:53:41 -0500

    update

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -13,7 +13,7 @@ fi
 
 if [ -e "$(which git)" ]; then
     # clean 'dirty' status of touched files that haven't been modified
-    git diff >/dev/null 2>/dev/null 
+    git diff >/dev/null 2>/dev/null
 
     # get a string like "v0.6.0-66-g59887e8-dirty"
     DESC="$(git describe --dirty 2>/dev/null)"
@@ -33,3 +33,4 @@ if [ "$INFO" != "$NEWINFO" ]; then
     echo "$NEWINFO" >"$FILE"
     echo "#define BUILD_DATE \"$TIME\"" >>"$FILE"
 fi
+
